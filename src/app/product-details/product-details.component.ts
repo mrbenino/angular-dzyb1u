30cf89c7791b03a5abb8d820cfc468db3d42a422
mrbenino <beninbenino@gmail.com>
2020-11-29 16:32:08 +0200

    Adding navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { products } from '../products';
 import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  
+  product;
 
   constructor(
     private route: ActivatedRoute,
@@ -21,6 +23,9 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
   }
 
 }
